98749cceb40d9e5dfb45b53d5552fe524f37a71e
Oreilly Lab <lab@oreilly.com>
2025-01-13 12:50:13 +0000

    additional resources

--- terraform_scripts_aws/main.tf
+++ terraform_scripts_aws/main.tf
@@ -1,3 +1,17 @@
-resource {
+resource "aws_vpc" "test_vpc"{
   cidr_block = "10.0.0.1/16"
 }
+
+resource "aws_subnet" "test_subnet" {
+  vpc_id = aws_vpc.test_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1"
+}
+
+resource "aws_security_group" "test_security_group" {
+  vpc_id = aws_vpc.test_vpc.id
+  name = "Allow tls"
+  description = "Allow tls inbound traffic"
+}
+
+
